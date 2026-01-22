960f31244dbb029aa4f2318b4ae072d55a7f7349
samtheman19 <makin.samuel@gmail.com>
2026-01-22 12:42:28 +0000

    Create WorkoutDetails.js

---
+++ WorkoutDetails.js
@@ -0,0 +1,19 @@
+export default function WorkoutDetails() {
+  return (
+    <div className="card">
+      <h3>Warm-up</h3>
+      <ul>
+        <li>10 min easy</li>
+        <li>3 × 20s strides</li>
+      </ul>
+
+      <h3>Tempo / Steady</h3>
+      <ul>
+        <li>Incline: 1.0%</li>
+        <li>10 min easy @ 11.0–12.0 km/h</li>
+        <li>10–15 min steady @ 13.0–14.0 km/h</li>
+        <li>5 min easy cool down</li>
+      </ul>
+    </div>
+  );
+}
