99243edc6445baf6b309511911cd7f65b47c1043
samtheman19 <makin.samuel@gmail.com>
2026-01-22 12:41:24 +0000

    Create WorkoutHeader.js

---
+++ WorkoutHeader.js
@@ -0,0 +1,11 @@
+export default function WorkoutHeader() {
+  return (
+    <div className="header">
+      <div>
+        <h2>Sat – Tempo / Steady</h2>
+        <p>Mode: Treadmill</p>
+      </div>
+      <button className="reset-btn">Reset Week</button>
+    </div>
+  );
+}
