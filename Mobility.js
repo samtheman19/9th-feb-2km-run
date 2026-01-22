4f514ec1835e16343a8c03af55ed5bbb00f5837f
samtheman19 <makin.samuel@gmail.com>
2026-01-22 12:43:08 +0000

    Create Mobility.js

---
+++ Mobility.js
@@ -0,0 +1,12 @@
+export default function Mobility() {
+  return (
+    <div className="card">
+      <h3>Mobility</h3>
+      <div className="mobility-item">
+        <span>Glute stretch (per side)</span>
+        <span>01:00</span>
+        <button>▶</button>
+      </div>
+    </div>
+  );
+}
